12a820940c5f1cb7365a5758d74483af417ad69d
chenwei <1772529785@qq.com>
2020-08-17 21:54:51 +0800

    xiugai

--- src/main/java/com/wyh/springbootmybatisdemo/controller/UserController.java
+++ src/main/java/com/wyh/springbootmybatisdemo/controller/UserController.java
@@ -3,10 +3,8 @@ package com.wyh.springbootmybatisdemo.controller;
 
 import com.wyh.springbootmybatisdemo.mapper.UserMapper;
 import com.wyh.springbootmybatisdemo.pojo.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -18,6 +16,20 @@ public class UserController {
 
     @GetMapping
     public User findUser(@RequestParam(value = "id") Long id){
+
         return userMapper.findById(id);
     }
+
+    @GetMapping("/login")
+    public String jdsf(@RequestParam(value = "userName") String userName,@RequestParam(value = "passwd") String pw){
+        return "用户名不存在或密码不正确";
+    }
+
+    @PostMapping(value = "login",produces= MediaType.APPLICATION_JSON_VALUE)
+    public String fds(@RequestBody  User user){
+        return "登录成功";
+
+    }
+
+
 }
